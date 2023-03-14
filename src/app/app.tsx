dcdafa8d1662a53dba8a39a009c1ebd8f097a38a
David Docekal <dd.docekal@gmail.com>
2023-03-14 18:43:45 +0100

    Initial commit - set app, create api, connect api with client

--- src/app/app.tsx
+++ src/app/app.tsx
@@ -1,9 +1,15 @@
 // eslint-disable-next-line @typescript-eslint/no-unused-vars
 import styles from './app.module.css';
+import { useEffect } from 'react';
 
 import NxWelcome from './nx-welcome';
 
 export function App() {
+  useEffect(() => {
+    fetch('/api')
+      .then(t => t.json())
+      .then((d) => console.log(d))
+  }, []);
   return (
     <>
       <NxWelcome title="priklad-sporitelna" />
