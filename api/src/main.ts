e6e7d34a11ec64d0f4bdfd25343de715c483de8e
David Docekal <dd.docekal@gmail.com>
2023-03-16 08:13:33 +0100

    Add cache to api and allow api request

--- api/src/main.ts
+++ api/src/main.ts
@@ -7,15 +7,43 @@ import express from 'express';
 import * as path from 'path';
 
 const app = express();
+const axios = require("axios");
+const apicache = require("apicache")
+const cache = apicache.middleware
 
 app.use('/assets', express.static(path.join(__dirname, 'assets')));
 
-app.get('/api', (req, res) => {
-  res.send({ message: 'Welcome to api!' });
+app.use(cache('5 minutes'))
+
+app.get("/api", async (req, res) => {
+  try {
+    const drinkPromise = axios.get("https://www.thecocktaildb.com/api/json/v1/1/search.php?s=margarita")
+    const banksPromise = axios.get("https://webapi.developers.erstegroup.com/api/csas/public/sandbox/v3/places", { headers: { "WEB-API-key": "58861fa4-1659-4e02-b7d8-847036c094fb" }})
+
+    const [drinkResponse, banksResponse] = await Promise.all([drinkPromise, banksPromise]);
+
+    const { data : {drinks = []} } = drinkResponse;
+    const { data: {items: banks = []} } = banksResponse;
+
+    const response = {
+      status: 200,
+      message: "success",
+      data: {
+        drink: drinks[0],
+        banks: banks.filter((bank: any) => bank.city === 'Brno' && bank.type === 'BRANCH' && bank.state === 'OPEN'),
+      },
+    };
+
+    return res.json(response);
+  } catch (err) {
+    return res.json({ message: err, status: res.statusCode });
+  }
 });
 
 const port = process.env.PORT || 3333;
+
 const server = app.listen(port, () => {
   console.log(`Listening at http://localhost:${port}/api`);
 });
+
 server.on('error', console.error);
